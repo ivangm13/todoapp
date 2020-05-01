3af30f305ac32d78ff7784d5fd95c2dfbf514040
ivangm13 <ivangarcia346@gmail.com>
2020-05-01 18:28:37 +0200

    modificaciones en la maquetación. Añadidos colores a las tareas según prioridad

--- js/main.js
+++ js/main.js
@@ -3,34 +3,48 @@ var tareas = document.querySelector('.tareas');
 
 listaTareas = [
     {
-        'idtarea': 0,
-        'texto': 'Estudiar',
-        'prioridad': 'urgente'
+        idtarea: 0,
+        texto: 'Estudiar',
+        prioridad: 'urgente'
     },
     {
-        'idtarea': 1,
-        'texto': 'Ir a por el pan',
-        'prioridad': 'diaria'
+        idtarea: 1,
+        texto: 'Ir a por el pan',
+        prioridad: 'diaria'
     },
     {
-        'idtarea': 2,
-        'texto': 'Hacer la compra',
-        'prioridad': 'semanal'
+        idtarea: 2,
+        texto: 'Hacer la compra',
+        prioridad: 'mensual'
     },
     {
-        'idtarea': 3,
-        'texto': 'Pasear al perro',
-        'prioridad': 'diaria'
+        idtarea: 3,
+        texto: 'Pasear al perro',
+        prioridad: 'diaria'
     },
 
 ];
 
 function mostrarTareas(pListaTareas) {
+    let color = "";
 
-    for(tarea of pListaTareas){
-        tareas.innerHTML += `<div class="tarea">
+    for (tarea of pListaTareas) {
+        switch (tarea.prioridad) {
+            case 'diaria':
+                color = 'chartreuse'
+                break;
+            case 'mensual':
+                color = 'blue'
+                break;
+            case 'urgente':
+                color = 'tomato'
+                break;
+        }
+        tareas.innerHTML += `<div class="tarea ${color}">
                                 <div class="texto"> ${tarea.texto}</div>
                                 <div class="eliminar">Eliminar</div>
-                            </div>`
+                            </div>`;
+        
     }
-}
+}//Fin mostrarTareas
+mostrarTareas(listaTareas);
