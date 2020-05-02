8f9d1ec83155ed47c2158bbcdbc2fc6a7be02a39
ivangm13 <ivangarcia346@gmail.com>
2020-05-02 17:25:11 +0200

    Añadidos filtros por prioridad y por texto de la tarea, ambos reaccionan al cambio

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 var listaTareas = new Array();
 var tareas = document.querySelector('.tareas');
-
+var idTareasGlobal = 4;
 listaTareas = [
     {
         idtarea: 0,
@@ -27,6 +27,7 @@ listaTareas = [
 
 function mostrarTareas(pListaTareas) {
     let color = "";
+    tareas.innerHTML = "";
 
     for (tarea of pListaTareas) {
         switch (tarea.prioridad) {
@@ -40,11 +41,86 @@ function mostrarTareas(pListaTareas) {
                 color = 'tomato'
                 break;
         }
-        tareas.innerHTML += `<div class="tarea ${color}">
+        tareas.innerHTML += `<div class="tarea ${color}" id="${tarea.idtarea}">
                                 <div class="texto"> ${tarea.texto}</div>
                                 <div class="eliminar">Eliminar</div>
                             </div>`;
-        
+
     }
 }//Fin mostrarTareas
 mostrarTareas(listaTareas);
+
+//Funcion para añadir tareas
+
+var btnGuardar = document.querySelector('#guardar');
+
+
+btnGuardar.addEventListener('click', crearTarea);
+
+function crearTarea() {
+    var tareaNueva = document.querySelector('#crearTarea').value;
+    var prioridadNuevaTarea = document.querySelector('#prioridad').value;
+    let tarea = {
+        idtarea: idTareasGlobal,
+        texto: tareaNueva,
+        prioridad: prioridadNuevaTarea
+    };
+    listaTareas.push(tarea);
+    mostrarTareas(listaTareas);
+    idTareasGlobal++;
+}
+
+//Funcion para eliminar tareas
+
+var btnEliminar = document.querySelectorAll('.tarea .eliminar');
+for (btn of btnEliminar) {
+    btn.addEventListener('click', eliminarTarea);
+}
+
+function eliminarTarea() {
+
+    let idTareaEliminar = document.getElementsByClassName('tarea');
+    console.log(idTareaEliminar);
+
+}
+
+
+//Filtros
+
+//Filtrar por prioridad
+
+var filtroPrioridad = document.querySelector('.buscar #selector2');
+
+filtroPrioridad.addEventListener('change', filtrarPrioridad);
+
+function filtrarPrioridad() {
+    let listaFiltrada = new Array();
+    for (tarea of listaTareas) {
+        if (tarea.prioridad == (filtroPrioridad.value).toLowerCase()) {
+            listaFiltrada.push(tarea);
+        }
+    }
+    mostrarTareas(listaFiltrada);
+}
+
+//Filtrar por texto (debe reaccionar al poner cada letra)
+
+var buscarTarea = document.querySelector('.buscar #buscadorTarea');
+
+buscarTarea.addEventListener('keyup', filtrarTareas);
+
+function filtrarTareas() {
+    let listaFiltrada = new Array();
+    
+    let textoIntroducido = document.querySelector('.buscar #buscadorTarea').value.toLowerCase();
+    for(tarea of listaTareas){
+        let texto = tarea.texto.toLowerCase();
+        console.log(texto);
+        console.log(textoIntroducido);
+        if(texto.includes(textoIntroducido)){
+            listaFiltrada.push(tarea);
+        }
+    }
+    console.log(listaFiltrada)
+    mostrarTareas(listaFiltrada)
+}
